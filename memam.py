dbb1e31a293381faf0a0eca0c6b1a3b62d84fb94
Reddy-Vamsi <reddy.vamsi@hotmail.com>
2020-11-19 19:53:27 -0500

    description

---
+++ memam.py
@@ -0,0 +1,8 @@
+'''
+Software Carpentry, Fall 2020
+"MEMAM"
+Medical Emergencies Management and Alert mechanism for corporate hospitals
+Submitted by
+Vamsi Reddy <vreddy17@jhu.edu>
+Emad Mohammed Naveed <enaveed1@jhu.edu>
+'''
